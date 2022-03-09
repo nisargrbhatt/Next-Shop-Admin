2f738ab35c6d34443ad33d176fb8877c6404e107
nisargrbhatt <nisargrbhatt@gmail.com>
2022-03-09 18:32:17 +0530

    Shared changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { RouterModule } from '@angular/router';
 import { AngularMaterialModule } from './../angular-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-// import { SidenavComponent } from './sidenav/sidenav.component';
 import { HeaderComponent } from './header/header.component';
 import { ErrorComponent } from './dialog/error/error.component';
 import { ResMesComponent } from './dialog/res-mes/res-mes.component';
@@ -11,7 +10,6 @@ import { LoaderComponent } from './loader/loader.component';
 import { ConfirmComponent } from './dialog/confirm/confirm.component';
 import { KycDeclineComponent } from './dialog/kyc-decline/kyc-decline.component';
 import { ProductCardSmallComponent } from './product/product-card-small/product-card-small.component';
-// import { ProductCardLongComponent } from './product/product-card-long/product-card-long.component';
 import { CustomerListTableComponent } from './payment/customer-list-table/customer-list-table.component';
 import { PaymentListTableComponent } from './payment/payment-list-table/payment-list-table.component';
 import { OrderListTableComponent } from './payment/order-list-table/order-list-table.component';
@@ -19,7 +17,6 @@ import { BarLineChartComponent } from './charts/bar-line-chart/bar-line-chart.co
 @NgModule({
   imports: [CommonModule, AngularMaterialModule, RouterModule],
   declarations: [
-    // SidenavComponent,
     HeaderComponent,
     ErrorComponent,
     ResMesComponent,
@@ -28,7 +25,6 @@ import { BarLineChartComponent } from './charts/bar-line-chart/bar-line-chart.co
     ConfirmComponent,
     KycDeclineComponent,
     ProductCardSmallComponent,
-    // ProductCardLongComponent,
     CustomerListTableComponent,
     PaymentListTableComponent,
     OrderListTableComponent,
@@ -36,7 +32,6 @@ import { BarLineChartComponent } from './charts/bar-line-chart/bar-line-chart.co
   ],
   providers: [],
   exports: [
-    // SidenavComponent,
     HeaderComponent,
     ErrorComponent,
     ResMesComponent,
@@ -45,7 +40,6 @@ import { BarLineChartComponent } from './charts/bar-line-chart/bar-line-chart.co
     ConfirmComponent,
     KycDeclineComponent,
     ProductCardSmallComponent,
-    // ProductCardLongComponent,
     CustomerListTableComponent,
     PaymentListTableComponent,
     OrderListTableComponent,
